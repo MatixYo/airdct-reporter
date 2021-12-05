87c06e1cd5138ffebc1beab518914f63872e5754
MatixYo <matixovi@gmail.com>
2021-12-05 13:38:53 +0100

    Use more efficient watch

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
 const dump1090file = '/run/dump1090-fa';
-fs.watchFile(dump1090file, (curr, prev) => {
-  console.log(`The current mtime is: ${curr.mtime}`);
-  console.log(`The previous mtime was: ${prev.mtime}`);
-  console.log(`The file has been modified`);
+
+fs.watch(dump1090file, (event, filename) => {
+  if (filename) {
+    console.log(`${filename} file Changed`);
+  }
 });
