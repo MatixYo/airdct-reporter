e64a898bd280935f45bf7d24877e2a0795a6010c
MatixYo <matixovi@gmail.com>
2021-12-05 14:44:55 +0100

    Send aircraft data to apis

--- README.md
+++ README.md
@@ -3,4 +3,8 @@
 1. Install Node using [NVM](https://github.com/nvm-sh/nvm).
 2. SSH into PI. Go to `~` directory and run following `git clone https://github.com/MatixYo/airdct-reporter.git`
 3. Run `npm install` and `npm run start` to verify it works
-4. Install PM2 to run the app in the background.
+4. Install PM2 to run the app in the background: `sudo npm install -g pm2 `
+5. Run `pm2 start index.js` to start the app in the background
+6. Save `pm2 save`
+7. Run `pm2 startup`
+8. You will see output with instructions. You still have to follow the instructions to make pm2 start automatically on reboot.
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
 require('dotenv').config();
+const fetch = require('node-fetch');
 
 const dump1090Folder = process.env.DUMP1090_FOLDER;
 const aircraftFilename = 'aircraft.json';
+const apis = process.env.filter(x => x.startsWith('API_URL_'));
+console.log(apis)
 
 fs.watch(dump1090Folder, (event, filename) => {
   if(filename !== aircraftFilename) return;
   const fileContent = fs.readFileSync(`${dump1090Folder}/${aircraftFilename}`);
-  const data = JSON.parse(fileContent);
+  const body = JSON.parse(fileContent);
+
+  apis.forEach((api) => {
+    fetch(api, {
+      body
+    });
+  })
   console.log(`${filename}`, data.aircraft.length);
 });
