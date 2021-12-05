e0c1febd5e87068d8f3c00c7e08a141aae0cbf01
MatixYo <matixovi@gmail.com>
2021-12-05 14:02:09 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+##Installation
+
+1. Install Node using [NVM](https://github.com/nvm-sh/nvm).
+2. SSH into PI. Go to `~` directory and run following `git clone https://github.com/MatixYo/airdct-reporter.git`
+3. Run `npm install` and `npm run start` to verify it works
+4. Install PM2 to run the app in the background.
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 require('dotenv').config();
 
-const dump1090file = '/run/dump1090-fa/aircraft.json';
+const aircraftFilename = 'aircraft.json';
 
-fs.watch(process.env.DUMP1090_LOCATION, (event, filename) => {
+fs.watch(process.env.DUMP1090_FOLDER, (event, filename) => {
+  if(filename !== aircraftFilename) return;
   const fileContent = fs.readFileSync(dump1090file);
   const data = JSON.parse(fileContent);
   console.log(`${filename}`, data.aircraft.length);
