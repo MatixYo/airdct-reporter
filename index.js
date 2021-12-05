4dc2d061d6f1679a3e5c75015567c8c01797249a
MatixYo <matixovi@gmail.com>
2021-12-05 14:49:24 +0100

    Fix modules loading

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
-require('dotenv').config();
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const dump1090Folder = process.env.DUMP1090_FOLDER;
 const aircraftFilename = 'aircraft.json';
-const apis = process.env.filter(x => x.startsWith('API_URL_'));
-console.log(apis)
+
+const apis = Array.from(process.env).filter(x => x.startsWith('API_URL_'));
 
 fs.watch(dump1090Folder, (event, filename) => {
   if(filename !== aircraftFilename) return;
@@ -13,9 +14,9 @@ fs.watch(dump1090Folder, (event, filename) => {
   const body = JSON.parse(fileContent);
 
   apis.forEach((api) => {
+    console.log(`Submitting to ${api} ${data.aircraft.length} aircraft`);
     fetch(api, {
       body
     });
   })
-  console.log(`${filename}`, data.aircraft.length);
 });
