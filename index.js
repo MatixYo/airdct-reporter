d1760496392ce98f1a60007569fd6e879a1a5952
MatixYo <matixovi@gmail.com>
2021-12-05 13:41:25 +0100

    Show file content

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 
-const dump1090file = '/run/dump1090-fa';
+const dump1090file = '/run/dump1090-fa/aircraft.json';
 
 fs.watch(dump1090file, (event, filename) => {
-  if (filename) {
-    console.log(`${filename} file Changed`);
-  }
+  const data = fs.readFileSync(dump1090file);
+  console.log(`${filename} file Changed`, data);
 });
