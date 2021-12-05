397cf929c39adb05913a2c0ec8acad7542380464
MatixYo <matixovi@gmail.com>
2021-12-05 13:51:28 +0100

    Fix

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const dump1090file = '/run/dump1090-fa/aircraft.json';
 
 fs.watch(process.env.DUMP1090_LOCATION, (event, filename) => {
   const fileContent = fs.readFileSync(dump1090file);
-  const data = JSON.read(fileContent);
+  const data = JSON.parse(fileContent);
   console.log(`${filename}`, data.aircraft.length);
 });
