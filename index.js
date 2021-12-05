28101bd8e79554c63b3fc87f13cf78469ab82125
MatixYo <matixovi@gmail.com>
2021-12-05 14:04:13 +0100

    Fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 require('dotenv').config();
 
+const dump1090Folder = process.env.DUMP1090_FOLDER;
 const aircraftFilename = 'aircraft.json';
 
-fs.watch(process.env.DUMP1090_FOLDER, (event, filename) => {
+fs.watch(dump1090Folder, (event, filename) => {
   if(filename !== aircraftFilename) return;
-  const fileContent = fs.readFileSync(dump1090file);
+  const fileContent = fs.readFileSync(`${dump1090Folder}/${aircraftFilename}`);
   const data = JSON.parse(fileContent);
   console.log(`${filename}`, data.aircraft.length);
 });
