64f26436a5baeddc740b7b8ac8792e8cbb8cfd45
Mateusz Juszczyk <matixovi@gmail.com>
2022-07-07 09:52:50 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. Install NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install node
+nvm use node
+
+# 2. Install Git
+sudo apt update
+sudo apt install git
+
+# 2. Clone app and install dependencies
+cd ~
+git clone https://github.com/MatixYo/airdct-reporter.git
+cd airdct-reporter
+npm install
+
+# 3. Start app
+sudo npm install -g pm2
+pm2 start index.js
+pm2 save
+pm2 startup
